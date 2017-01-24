88b14da2a1e38de6d1c63e7370a1cabceece1fb1
John Shiner <john.shiner@gmail.com>
2017-01-24 10:49:30 -0800

    Added String() and Int() functions to val type

--- main.go
+++ main.go
@@ -16,16 +16,29 @@ func main() {
 
 	a, b := 9, 5
 
-	fmt.Println(x.Plus(0).V())          // 11 = 11 + 0
-	fmt.Println(y.Times(1).V())         //  9 = 9 * 1
-	fmt.Println(vals[0].Times(a).V())   // 99 = 11 * 9
-	fmt.Println(vals[0].Plus(b).V())    // 16 = 11 + 5
-	fmt.Println(vals[1].Times(a).V())   // 81 = 9 * 9
-	fmt.Println(vals[0].Times(4).V())   // 44 = 11 * 4
-	fmt.Println(z.Times(3).Plus(4).V()) // 99 = 33 * 3
+	fmt.Println(x.Plus(0))          // 11 = 11 + 0
+	fmt.Println(y.Times(1))         //  9 = 9 * 1
+	fmt.Println(vals[0].Times(a))   // 99 = 11 * 9
+	fmt.Println(vals[0].Plus(b))    // 16 = 11 + 5
+	fmt.Println(vals[1].Times(a))   // 81 = 9 * 9
+	fmt.Println(vals[0].Times(4))   // 44 = 11 * 4
+	fmt.Println(z.Times(3).Plus(4)) // 99 = 33 * 3
 
 	zz := val.NewVal(7)
 	HypotSquared := val.NewVal(a*a + b*b)
-	fmt.Println(HypotSquared.V())                                 // 106 = 9*9 + 5*5
-	fmt.Println(HypotSquared.Plus(zz.V()).Plus(-4).Times(10).V()) // 1090 = ((106 + 7 - 4)*10)
+	fmt.Println(HypotSquared)                                   // 106 = 9*9 + 5*5
+	fmt.Println(HypotSquared.Plus(zz.Int()).Plus(-4).Times(10)) // 1090 = ((106 + 7 - 4)*10)
 }
+
+/***** Output *****
+[ `go run main.go` | done: 206.294017ms ]
+	11
+	9
+	99
+	16
+	81
+	44
+	103
+	106
+	1090
+*******************/
--- val/val.go
+++ val/val.go
@@ -1,13 +1,15 @@
 package val
 
+import "strconv"
+
 type Val struct {
 	a int
 }
 
 /***********
 type interface Operator {
-	(c *Calc) Plus(b int) int;
-	(c *Calc) Times(b int) int
+	(c *Val) Plus(b int) int;
+	(c *Val) Times(b int) int
 }
 ************/
 
@@ -24,5 +26,13 @@ func (v *Val) Times(b int) *Val {
 }
 
 func (v *Val) V() int {
+	return v.a
+}
+
+func (v Val) String() string {
+	return strconv.Itoa(v.a) // convert int to string
+}
+
+func (v Val) Int() int {
 	return v.a
 }
