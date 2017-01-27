ce51f5db549c60f4955cf5cfc5a1d9744b0ed4a9
John Shiner <john.shiner@gmail.com>
2017-01-27 12:28:37 -0800

    change fmt to log for test outputs

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
-import "fmt"
 import "./val"
+import "log"
 
 func main() {
 
@@ -16,18 +16,20 @@ func main() {
 
 	a, b := 9, 5
 
-	fmt.Println(x.Plus(0))          // 11 = 11 + 0
-	fmt.Println(y.Times(1))         //  9 = 9 * 1
-	fmt.Println(vals[0].Times(a))   // 99 = 11 * 9
-	fmt.Println(vals[0].Plus(b))    // 16 = 11 + 5
-	fmt.Println(vals[1].Times(a))   // 81 = 9 * 9
-	fmt.Println(vals[0].Times(4))   // 44 = 11 * 4
-	fmt.Println(z.Times(3).Plus(4)) // 103 = (33 * 3) + 4
+	log.Println(x.Plus(0))          // 11 = 11 + 0 ; change to Log functions
+	log.Println(y.Times(1))         //  9 = 9 * 1
+	log.Println(vals[0].Times(a))   // 99 = 11 * 9
+	log.Println(vals[0].Plus(b))    // 16 = 11 + 5
+	log.Println(vals[1].Times(a))   // 81 = 9 * 9
+	log.Println(vals[0].Times(4))   // 44 = 11 * 4
+	log.Println(z.Times(3).Plus(4)) // 103 = (33 * 3) + 4 ; test chained operations
+	log.Println(vals[2])            // 0 - test default initialized value
+	log.Println(val.NewVal(2))      // 2 - test constructor
 
 	zz := val.NewVal(7)
 	HypotSquared := val.NewVal(a*a + b*b)
-	fmt.Println(HypotSquared)                                   // 106 = 9*9 + 5*5
-	fmt.Println(HypotSquared.Plus(zz.Int()).Plus(-4).Times(10)) // 1090 = ((106 + 7 - 4)*10)
+	log.Println(HypotSquared)                                   // 106 = 9*9 + 5*5
+	log.Println(HypotSquared.Plus(zz.Int()).Plus(-4).Times(10)) // 1090 = ((106 + 7 - 4)*10)
 }
 
 /***** Output *****
