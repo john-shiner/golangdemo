bbbb00c4eefa55b0059dfaf76b12656734c69c00
John Shiner <john.shiner@accenture.com>
2017-02-15 14:38:46 -0800

    fixed tests case logic

--- val/val_test.go
+++ val/val_test.go
@@ -7,27 +7,23 @@ func TestVal(t *testing.T) {
 		name string
 		a    int
 		b    int
-		got  int
+		GotPlus  int
+		GotTimes int
 	}{
-		{"plus", 5, 4, 9},
-		{"times", 5, 4, 20},
+		{"first", 5, 4, 9, 20},
+		{"second", 6, 9, 15, 54},
 	}
 	for _, tst := range tests {
-		if tst.name == "plus" {
-			v := NewVal(tst.a)
-			got := v.Plus(tst.b).Int()
-			if got != tst.got {
-				t.Errorf("tst(%+v) = got(%v)", tst, got)
-			}
-			println("Plus %d, %d, %d, ok", tst.a, tst.b, tst.got)
+		v := NewVal(tst.a)
+		got := v.Plus(tst.b).Int()
+		if got != tst.GotPlus {
+			t.Errorf("tst(%+v) = got(%v)", tst, tst.GotPlus)
 		}
-		if tst.name == "times" {
-			v := NewVal(tst.a)
-			got := v.Times(tst.b).Int()
-			if got != tst.got {
-				t.Errorf("tst(%+v) = got(%v)", tst, got)
-			}
-			println("Times %d, %d, %d, ok", tst.a, tst.b, tst.got)
+		v2 := NewVal(tst.a)
+		got2 := v2.Times(tst.b).Int()
+		if got2 != tst.GotTimes {
+			t.Errorf("tst(%+v) = got(%v)", tst, tst.GotTimes)
 		}
 	}
 }
+
