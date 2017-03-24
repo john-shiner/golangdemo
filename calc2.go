56009689c654849283f226b0985336800376ee12
Shiner <john.shiner@accenture.com>
2017-03-24 14:04:09 -0500

    calc2.go - limit function and interacting calculator instances...  more to do

--- calc2.go
+++ calc2.go
@@ -23,6 +23,18 @@ func Add(a, b float64) float64 { return a + b }
 func Sub(a, b float64) float64 { return a - b }
 func Mul(a, b float64) float64 { return a * b }
 func Div(a, b float64) float64 { return a / b }
+func Max(a, b float64) float64 {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+// Consdider storing separate calculators and exchange model semantics
+// Max function is generic, but there are rules that should disable subsequent
+// calculations once some threshold has been reached.  This needs to be part of the
+// operation semantics (hence, instead of "Max()", it might be a "Limit()"
+// function).
 
 func main() {
 	fmt.Println("Go Calculation Engine Main")
@@ -32,10 +44,19 @@ func main() {
 		{Mul, 4},
 		{Sub, 2},
 		{Div, 2},
+		{Mul, 3},
 	}
-
+	maxEarnings2 := 48.0
+	x := 0.0
 	for i, v := range ops {
-		x := c.Do(v.f, v.v)
+		x = c.Do(v.f, v.v)
 		fmt.Printf("%d\t\t%6.1f\t\t%v\n", i, x, x)
 	}
+	r := x
+	fmt.Printf("original earnings\t\t%6.1f\n", r)
+	r1 := Add(c.acc, 5.0)
+	r2 := c.Do(Max, maxEarnings2)
+	r3 := c.Do(Add, 5.0)
+	r4 := Max(c.acc, 52.0)
+	fmt.Printf("newearnings\t\t%6.1f, \t\t%6.1f, \t\t%6.1f, \t\t%6.1f, \t\t%6.1f, %6.1f\n", c.acc, r, r1, r2, r3, r4)
 }
